719928625dc0b265d6e0f1d88fa4c3e2dd6a99f3
taralizee <tara.minchin@unine.ch>
2026-05-14 12:13:30 +0200

    matrixcreated

--- Project/scripts/Create_matrix.r
+++ Project/scripts/Create_matrix.r
@@ -3,8 +3,8 @@
 library(tidyverse)
 
 # 1. Load files
-hits <- read_tsv("../outputs/abricate/strain_gene_matches_from_raws.tsv", show_col_types = FALSE)
-metadata <- read_csv("../Strains_cleaned.csv", show_col_types = FALSE)
+hits <- read_tsv("Project/outputs/abricate/strain_gene_matches_from_raws.tsv", show_col_types = FALSE)
+metadata <- read_csv2("Project/Strains_cleaned.csv", show_col_types = FALSE)
 
 # 2. Clean ABRicate table
 hits_clean <- hits %>%
@@ -33,9 +33,13 @@ gene_matrix <- gene_matrix %>%
   mutate(total_genes_detected = mecA + sdrG + ica_operon)
 
 # 5. Merge with original metadata
-# Uses strain name to merge 
+# Normalize strain names: replace spaces with underscores to match merged file format
+# Keep only first occurrence of each strain (remove duplicates from metadata)
+metadata <- metadata %>%
+  distinct(Strain, .keep_all = TRUE) %>%
+  mutate(Strain = str_replace_all(Strain, " ", "_"))
 final_table <- metadata %>%
-  left_join(gene_matrix, by = "strains) %>%
+  left_join(gene_matrix, by = c("Strain" = "strain")) %>%
   mutate(
     across(c(mecA, sdrG, ica_operon, total_genes_detected),
            ~ replace_na(.x, 0))
@@ -46,5 +50,5 @@ final_table_with_hits <- final_table %>%
   filter(total_genes_detected > 0)
 
 # 7. Save output
-write_csv(final_table_with_hits, "strains_with_genes.csv")
-write_csv(gene_matrix, "gene_presence_absence_matrix.csv")
+write_csv(final_table_with_hits, "Project/outputs/strains_with_genes.csv")
+write_csv(gene_matrix, "Project/outputs/gene_presence_absence_matrix.csv")
